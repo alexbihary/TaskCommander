d732258aa59fe06636d91824fc48711c0c854c7e
Alexander Bihary <alex.bihary@gmail.com>
2013-08-09 11:58:26 -0400

    sample help text

--- TaskCommander.SampleApp/HelloWorldTask.cs
+++ TaskCommander.SampleApp/HelloWorldTask.cs
@@ -4,7 +4,10 @@ using System.Collections.Generic;
 
 namespace TaskCommander.SampleApp
 {
-    [Task(Name="hello", Description="Starter task that simply prints HelloWorld.")]
+    [Task(Name="hello", 
+        Description="Starter task that simply prints HelloWorld.",
+        Help="hello [<options>]\n" +
+             "      -name:<value>   Outputs 'Hello <value>'")]
     public class HelloWorldTask : ITask
     {
         public Prompt Run(IDictionary<string, string> args, IConsole console)
